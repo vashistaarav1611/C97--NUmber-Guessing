c22d29677fe67bc97729e4a86611270032ab4789
SamriddhiShah24 <77601757+SamriddhiShah24@users.noreply.github.com>
2021-07-19 13:19:38 +0530

    Add files via upload

---
+++ guessNumber.py
@@ -0,0 +1,20 @@
+print('Number Guessing Game')
+print('Enter a number between 1-10')
+
+
+firstGuess=input('Enter a number between 1-10')
+
+
+if(firstGuess==6):
+    print("Superb! You guessed the Number !!")
+elif(firstGuess<4):
+    print("Too low, try a higher number...")
+elif(firstGuess==5):
+    print("Close... Keep trying")
+elif(firstGuess==6):
+    print("Close... Keep trying")
+elif(firstGuess>7):
+    print("Too far, try a lower number")
+
+
+
